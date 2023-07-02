afd07c0e9466b6479f76b577805e1fd9c377fcd8
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2023-07-01 20:24:49 -0600

    corrigiendo texto

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -8,7 +8,7 @@ export default function HireMe() {
         <CircularText className="fill-dark animate-spin-slow dark:fill-light" />
         <Link
           href="/"
-          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-24 h-24 rounded-full font-semibold hover:bg-light hover:text-dark  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light  dark:fill-dark fill-light hover:fill-dark hover:dark:fill-light dark:hover:border-light md:w-12 md:h-12 md:text-[10px] md:absolute"
+          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-24 h-24 rounded-full font-semibold hover:bg-light hover:text-dark  dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light  dark:fill-dark fill-light hover:fill-dark hover:dark:fill-light dark:hover:border-light md:w-12 md:h-12 md:text-[8px] md:absolute"
         >
           Contacto
         </Link>
