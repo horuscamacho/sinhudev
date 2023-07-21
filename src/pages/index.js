ed14bdabc749152307ba2a5e8d28d1cc591f508b
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2023-07-21 15:09:47 -0600

    cambianod la imágen

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 import Image from "next/image";
-import profilePic from "../../public/images/profile/developer-pic-1.png";
+import profilePic from "../../public/images/avatar.jpg";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { ArrowCV } from "@/components/Icons";
